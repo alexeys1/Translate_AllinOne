773434405abbcd171af32bffbf2d7b91a03e224f
Alexeys <alexeys@dfacg.com>
2026-08-10 01:00:16 +0800

    keep translation failures out of tooltips

--- src/main/java/com/cedarxuesong/translate_allinone/utils/translate/TooltipInternalLineSupport.java
+++ src/main/java/com/cedarxuesong/translate_allinone/utils/translate/TooltipInternalLineSupport.java
@@ -74,7 +74,7 @@ public final class TooltipInternalLineSupport {
     }
 
     public static boolean shouldShowErrorStatusLine(TooltipTranslationSupport.TooltipProcessingResult processedTooltip) {
-        return processedTooltip != null && !processedTooltip.errorMessage().isBlank();
+        return false;
     }
 
     public static List<Component> appendStatusLineIfNeeded(
